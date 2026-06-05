e57dde892d6c9d17e4b48d4441127d55fd7d1c3c
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-05 11:02:52 +0530

    Create Number pyramid .c

---
+++ Day 10/Number pyramid .c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 1; i <= 5; i++) {
+
+        // Print spaces
+        for(j = 1; j <= 5 - i; j++)
+            printf(" ");
+
+        // Ascending numbers
+        for(j = 1; j <= i; j++)
+            printf("%d", j);
+
+        // Descending numbers
+        for(j = i - 1; j >= 1; j--)
+            printf("%d", j);
+
+        printf("\n");
+    }
+
+    return 0;
+}
