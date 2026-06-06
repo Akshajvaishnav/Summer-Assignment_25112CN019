c17b410ba69adf6e85e6f4da61d83fe7d65edb7b
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-06 08:21:01 +0530

    Create Second Largest Element .c

---
+++ Day 14/Second Largest Element .c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int first = INT_MIN, second = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second Largest = %d", second);
+
+    return 0;
+}
