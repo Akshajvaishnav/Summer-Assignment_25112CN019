cc8175a755a657cf0e72d7e261f32ad52a9cd3f9
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:21:53 +0530

    Update To find sum of a digit of a number .c

--- Day 2 /To find sum of a digit of a number .c
+++ Day 2 /To find sum of a digit of a number .c
@@ -1,23 +1,16 @@
 #include <stdio.h>
-
 int main() {
     int num, sum = 0, remainder;
-
     printf("Enter an integer: ");
     scanf("%d", &num);
-
-    // Make the number positive if it's negative
     if (num < 0) {
         num = -num;
     }
-
     while (num != 0) {
-        remainder = num % 10;   // Get the last digit
-        sum = sum + remainder;  // Add it to the sum
-        num = num / 10;         // Remove the last digit
+        remainder = num % 10;   
+        sum = sum + remainder;  
+        num = num / 10;        
     }
-
     printf("Sum of digits = %d\n", sum);
-
     return 0;
 }
