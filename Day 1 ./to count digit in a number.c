c2fec79beb27c1ae13090b10958f42afedb00ddd
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:35:11 +0530

    Update to count digit in a number.c

--- Day 1 ./to count digit in a number.c
+++ Day 1 ./to count digit in a number.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
-
 int main() {
     long long n;
     int count = 0;
-
     printf("Enter an integer: ");
     scanf("%lld", &n);
-
-    // Handle 0 explicitly
     if (n == 0) {
         count = 1;
     } else {
-        // Remove negative sign if number is negative
         if (n < 0) n = -n;
-
         while (n != 0) {
-            n /= 10; // Remove the last digit
+            n /= 10; 
             count++;
         }
     }
-
     printf("Number of digits: %d\n", count);
     return 0;
 }
