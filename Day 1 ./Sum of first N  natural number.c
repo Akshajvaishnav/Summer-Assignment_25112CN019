76f87ebb35223a84d24259a17b8698bf787124e0
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:27:54 +0530

    Update Sum of first N  natural number.c

--- Day 1 ./Sum of first N  natural number.c
+++ Day 1 ./Sum of first N  natural number.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
 int main() {
     int n, sum;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-
-    // Constant time calculation O(1)
     sum = n * (n + 1) / 2;
-
     printf("Sum of first %d natural numbers = %d\n", n, sum);
     return 0;
 }
