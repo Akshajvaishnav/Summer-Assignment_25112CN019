60b719f5bf127ddb708f3164b3386745b8f2b57a
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:43:55 +0530

    Update To find Nth fibonacci term .c

--- Day 4/To find Nth fibonacci term .c
+++ Day 4/To find Nth fibonacci term .c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-// Function to find the nth Fibonacci number
+
 unsigned long long get_nth_fibonacci(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
 
-    unsigned long long prev2 = 0; // Represents F(n-2)
-    unsigned long long prev1 = 1; // Represents F(n-1)
+    unsigned long long prev2 = 0; 
+    unsigned long long prev1 = 1;
     unsigned long long current = 0;
 
     for (int i = 2; i <= n; i++) {
