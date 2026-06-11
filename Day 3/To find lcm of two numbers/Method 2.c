578c9b26263e279b1390d1bb0281d5922e43c604
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:38:40 +0530

    Update Method 2.c

--- Day 3/To find lcm of two numbers/Method 2.c
+++ Day 3/To find lcm of two numbers/Method 2.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-
 int main() {
     int num1, num2, max;
-
     printf("Enter two positive integers: ");
     scanf("%d %d", &num1, &num2);
-
-    // Store the larger number in max
     max = (num1 > num2) ? num1 : num2;
-
-    // Loop until max is perfectly divisible by both numbers
     while (1) {
         if (max % num1 == 0 && max % num2 == 0) {
             printf("The LCM of %d and %d is %d.\n", num1, num2, max);
             break; 
         }
         max++;
-        // Alternatively, use: max += (num1 > num2) ? num1 : num2; for optimization
     }
-
     return 0;
 }
