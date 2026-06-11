d35c907e4c0bacceb41aea84ac11db2a72fc24d1
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:35:49 +0530

    Update to find the factorial of a number.c

--- Day 1 ./to find the factorial of a number.c
+++ Day 1 ./to find the factorial of a number.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
 int main() {
     int n, i;
     unsigned long long factorial = 1;
-
     printf("Enter an integer: ");
     scanf("%d", &n);
-
-    // Factorial is not defined for negative numbers
     if (n < 0) {
         printf("Error! Factorial of a negative number doesn't exist.");
     } else {
@@ -16,6 +12,5 @@ int main() {
         }
         printf("Factorial of %d = %llu\n", n, factorial);
     }
-
     return 0;
 }
