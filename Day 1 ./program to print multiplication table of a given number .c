dc36241f7c03ee4aa994a54e5ba489d685a55e79
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-11 20:29:49 +0530

    Update program to print multiplication table of a given number .c

--- Day 1 ./program to print multiplication table of a given number .c
+++ Day 1 ./program to print multiplication table of a given number .c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 int main() {
     int num, i;
-
-    // Ask the user to enter the number for the table
     printf("Enter an integer: ");
     scanf("%d", &num);
-
     printf("Multiplication Table for %d:\n", num);
-
-    // Loop from 1 to 10 to calculate and print results
     for (i = 1; i <= 10; ++i) {
         printf("%d * %d = %d\n", num, i, num * i);
     }
-
     return 0;
 }
