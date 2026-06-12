68059e3501f04f0f7861acc693346b326463148b
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-12 12:50:55 +0530

    Update To convert binary to decimal.c

--- Day 6/To convert binary to decimal.c
+++ Day 6/To convert binary to decimal.c
@@ -3,20 +3,18 @@
 int binaryToDecimal(int n) {
     int dec = 0;
 
-    // Initializing base value to 1, i.e 2^0
+  
     int base = 1;
     
-    // Extracting each digits of binary number
-    // and adding corresponding exponent of 2
+  
     while (n) {
         int last_digit = n % 10;
         n = n / 10;
 
-        // Multiplying the last digit with the base value
-        // and adding it to the decimal value
+       
         dec += last_digit * base;
 
-        // Updating the base value by multiplying it by 2
+     
         base = base * 2;
     }
 
