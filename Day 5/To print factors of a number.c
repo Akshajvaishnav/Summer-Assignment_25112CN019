8345f49cc75cd3fbedfde18f22075937add4377c
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-12 12:48:40 +0530

    Update To print factors of a number.c

--- Day 5/To print factors of a number.c
+++ Day 5/To print factors of a number.c
@@ -3,7 +3,7 @@
 int main() {
     int num;
 
-    // Ask the user to input a positive integer
+    
     printf("Enter a positive integer: ");
     if (scanf("%d", &num) != 1 || num <= 0) {
         printf("Please enter a valid positive integer.\n");
@@ -12,9 +12,9 @@ int main() {
 
     printf("Factors of %d are: ", num);
 
-    // Loop from 1 to the given number to find its factors
+    
     for (int i = 1; i <= num; i++) {
-        // If the remainder is 0, then 'i' is a factor
+      
         if (num % i == 0) {
             printf("%d ", i);
         }
