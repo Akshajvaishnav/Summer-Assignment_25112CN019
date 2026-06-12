e4ecbe0d50910bb3a52a971639b0ca16805f4906
Akshaj Tripathi <25112cn019@glbitm.ac.in>
2026-06-12 12:56:28 +0530

    Update To find factorial of a number using recursion .c

--- Day 7/To find factorial of a number using recursion .c
+++ Day 7/To find factorial of a number using recursion .c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function declaration
+
 unsigned long long factorial(int n);
 
 int main() {
@@ -9,7 +9,7 @@ int main() {
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
-    // Error handling for negative integers
+ 
     if (num < 0) {
         printf("Factorial is not defined for negative numbers.\n");
     } else {
@@ -19,12 +19,12 @@ int main() {
     return 0;
 }
 
-// Recursive function to find factorial
+
 unsigned long long factorial(int n) {
-    // Base case: factorial of 0 or 1 is 1
+    
     if (n == 0 || n == 1) {
         return 1;
     }
-    // Recursive case: n! = n * (n - 1)!
+   
     return n * factorial(n - 1);
 }
